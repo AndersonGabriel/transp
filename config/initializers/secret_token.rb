417ef686f7ca29cc896807290e32abaa91684fef
anderson <anderson@anderson-14A4.(none)>
2014-02-17 19:25:20 -0300

    transp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Transp::Application.config.secret_key_base = '2a5ca061400a5379a6da14ceef36064a6f68a72b0e2f2b9fade091a5b0ec6db10e15b95bc00ab5c73e96b627bb6f045c419d0b5bd339ea7b2e7f066f44f558e8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Transp::Application.config.session_store :cookie_store, key: '_transp_session'
